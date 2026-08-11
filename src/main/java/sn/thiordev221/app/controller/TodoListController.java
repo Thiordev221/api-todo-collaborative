36a9be7116111b4de418fb6a42c3e8b831f08317
Thiordev221 <thiorabdoulaye39@gmail.com>
2026-08-11 19:16:49 +0000

    test: test for the method getMyLists which is in TodoListServiceImpl

--- src/main/java/sn/thiordev221/app/controller/TodoListController.java
+++ src/main/java/sn/thiordev221/app/controller/TodoListController.java
@@ -1,7 +1,10 @@
 package sn.thiordev221.app.controller;
 
+import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
